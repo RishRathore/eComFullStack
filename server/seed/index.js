fe7b480615707b17893e3ad6fdfbbe82adb4ecb2
rishabh <rishrathore95@gmail.com>
2022-09-28 19:53:40 +0530

    seed one user

--- server/seed/index.js
+++ server/seed/index.js
@@ -1,13 +1,21 @@
 const Product = require("../models/product")
 const products = require("./products")
 
-const seedDB = async () => {
-  const count = await Product.count();
+const User = require("../models/user")
+const users = require("./users")
 
-  if (count === 0) {
+const seedDB = async () => {
+  const productCount = await Product.count();
+  const userCount = await User.count();
+
+  if (productCount === 0) {
     console.log('seeding products...')
     // await Product.deleteMany({});
     await Product.insertMany(products)
+  }
+
+  if (userCount === 0) {
+    await User.insertMany(users);
   }
 }
 
---
+++ server/seed/users.js
@@ -0,0 +1,7 @@
+module.exports = [
+  {
+    name: 'Test',
+    email: 'test@yopmail.com',
+    password: '123456',
+  }
+]
